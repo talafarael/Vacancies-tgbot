4cf4fd10e56142f4ad148e6b2126db42bbc96f8e
artem <artemk2504@gmail.com>
2025-02-20 13:30:38 +0200

    some change

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import requests
+import xml.etree.ElementTree as ET 
 from bs4 import BeautifulSoup
+
+from get_dou import Vacancies
 url = "https://jobs.dou.ua/vacancies/feeds"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
     "Referer": "https://jobs.dou.ua/",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
 }
-response = requests.get(url, headers=headers)
+response = requests.get(url, headers=headers) 
 response.raise_for_status()
 soup = BeautifulSoup(response.content, "xml")
 
-# Перевіряємо вміст
+
 print(soup.prettify())
 
-# Спробуємо знайти потрібний елемент
+root = ET.fromstring(response.content)                   
+
+vacancies_body=root.find(".//div[@class='job-lit']")
+if(vacancies_body):
+    for item in vacancies_body.findall(".//div[@class='job-lit']"):
+        print("suka")
 job_lit_div = soup.find("div", {"class": "job-lit"})
 
 if job_lit_div:
