38e11653902c05b8c76aba8627b88dc4d90a429c
artem <artemk2504@gmail.com>
2025-02-20 22:10:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import requests
-import xml.etree.ElementTree as ET 
+import re
 from bs4 import BeautifulSoup
-
-from get_dou import Vacancies
-url = "https://jobs.dou.ua/vacancies/feeds"
+url = "https://djinni.co/jobs/?primary_keyword=Node.js"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
     "Referer": "https://jobs.dou.ua/",
@@ -11,20 +9,9 @@ headers = {
 }
 response = requests.get(url, headers=headers) 
 response.raise_for_status()
-soup = BeautifulSoup(response.content, "xml")
-
-
-print(soup.prettify())
-
-root = ET.fromstring(response.content)                   
-
-vacancies_body=root.find(".//div[@class='job-lit']")
-if(vacancies_body):
-    for item in vacancies_body.findall(".//div[@class='job-lit']"):
-        print("suka")
-job_lit_div = soup.find("div", {"class": "job-lit"})
-
-if job_lit_div:
-    print(job_lit_div.prettify())  # Виводимо красиво форматований HTML/XML
+soup = BeautifulSoup(response.content, "html.parser")
+pattern = re.compile(r'^job-item-\d+$')
+for item in soup.find_all('li',id=pattern):
+        print(item)
 else:
-    print("Div with class 'job-lit' not found!")
+    print("")
