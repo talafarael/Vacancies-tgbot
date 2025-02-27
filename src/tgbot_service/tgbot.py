8cc3d97c55bf5bebd2d1dae0f4ec27962f80036a
artem <artemk2504@gmail.com>
2025-02-27 18:45:34 +0200

    user create

--- src/tgbot_service/tgbot.py
+++ src/tgbot_service/tgbot.py
@@ -10,3 +10,4 @@
 class TgBot:
    def __init__(self,cluster) -> None:
         self.cluster=cluster
+
--- src/user_manager/user.py
+++ src/user_manager/user.py
@@ -1,11 +1,22 @@
 
 
 
-
-
 class User:
-   def __init__(self,cluster) -> None:
-        self.cluster=cluster
-   async def user_create(self,id:str):
-        user
+    def __init__(self, cluster) -> None:
+        self.cluster = cluster
 
+    async def user_create(self, chat_id: str, user_id: str):
+        try:
+            collection = self.cluster.test.user
+            user = {
+                "chat_id": chat_id,
+                "user_id": user_id,
+                "category": [],
+                "experience_year": [],
+                "language_level": []
+            }
+            res = await collection.insert_one(user)
+            return res.inserted_id  
+        except Exception as e:
+            print(f"Ошибка при выполнении запроса: {e}")
+            return None 
